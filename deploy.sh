da635bde42e4c72e52ddf01eab68ac39cb91e4c4
Ken Sheridan <kensheridan86@gmail.com>
2023-08-10 19:02:30 +0100

    basic config.yml and deploy,sh file added

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#run updates
+sudo apt update
+# Delete the old project folder
+# sudo rm -r /Fit4Life
+# Clone in new repo
+# git clone https://github.com/KSheridan86/Fit4Life.git
+# Change directory into the application folder
+cd ruby-backend
+# Pass in certs for HTTPS
+# echo $PRIVATE_KEY > privatekey.pem
+# echo $SERVER > server.crt
+# Start the server
+rails server --binding 0.0.0.0 -p 4000
