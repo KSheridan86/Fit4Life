868717fe9bfba6d3fedfe04306f30fd1284a39c7
Ken Sheridan <kensheridan86@gmail.com>
2023-08-10 21:44:12 +0100

    tidied up deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #run updates
 sudo apt update
-# Delete the old project folder
-# sudo rm -r /Fit4Life
-# Clone in new repo
-# git clone https://github.com/KSheridan86/Fit4Life.git
-# Change directory into the application folder
+# install rvm
 curl -sSL https://get.rvm.io | bash
 source ~/.rvm/scripts/rvm
+# install ruby version 3.2.2 and set as default
 rvm install 3.2.2
 rvm use 3.2.2 --default
+# Change directory into the application folder
 cd Fit4Life
 cd ruby-backend
+# Run a migration 
 rails db:migrate RAILS_ENV=development
-
 # Pass in certs for HTTPS
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
