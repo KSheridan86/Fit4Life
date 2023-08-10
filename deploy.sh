2de22481de6e89da83f507bcddeb4489e1e1543a
Ken Sheridan <kensheridan86@gmail.com>
2023-08-10 20:26:35 +0100

    deploy.sh updated with a migration command

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ sudo apt update
 # Clone in new repo
 # git clone https://github.com/KSheridan86/Fit4Life.git
 # Change directory into the application folder
+cd Fit4Life
 cd ruby-backend
+rails db:migrate RAILS_ENV=development
+
 # Pass in certs for HTTPS
 # echo $PRIVATE_KEY > privatekey.pem
 # echo $SERVER > server.crt
